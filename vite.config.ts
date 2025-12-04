f0554cddc5a2c9949dec82238592ee11826dd217
linht <835196268@qq.com>
2025-12-04 17:44:39 +0800

    fix:跨域问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // 留空则不会设置 COOP/COEP，避免沙箱请求被浏览器拦截
+    // headers: {
+    //   'Cross-Origin-Opener-Policy': 'same-origin',
+    //   'Cross-Origin-Embedder-Policy': 'require-corp',
+    // },
+  },
 })
-
